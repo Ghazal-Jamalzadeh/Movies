389105b9c26252bbce5134a29e15192c3bce4be5
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2022-08-21 08:33:47 -0700

    54- project structure & packages

--- app/src/main/java/com/jmzd/ghazal/movies/ui/MainActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/movies/ui/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.jmzd.ghazal.movies
+package com.jmzd.ghazal.movies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jmzd.ghazal.movies.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
